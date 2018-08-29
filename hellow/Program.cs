a380b36fd6964c6b1a759447bc6766004775a1a6
Patrick Barnes <pdbarnes@csu.fullerton.edu>
2018-08-28 17:53:07 -0700

    ask questions, simple assignment

--- hellow/Program.cs
+++ hellow/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Program: hello world + input
+ * Author:  Patrick Barnes
+ * Date:    8/28/2018
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +13,28 @@ namespace isds309_hellow
 {
     class Program
     {
-        static void Pause()
-        {
-            Console.WriteLine("Press the anykey to continue.");
-            Console.ReadKey(true);
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world! again");
-            string input;
-            input = Console.ReadLine();
+            //variables
+            string name, class1, class2;
+            string class1grade, class2grade; //able to int later when convert
 
-            Console.WriteLine("input:" + input);
-            Pause();
+            //get info
+            Console.WriteLine("Please enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter your first class:");
+            class1 = Console.ReadLine();
+            Console.WriteLine("Please enter your first class grade:");
+            class1grade = Console.ReadLine();
+            Console.WriteLine("Please enter your second class:");
+            class2 = Console.ReadLine();
+            Console.WriteLine("Please enter your second class grade:");
+            class2grade = Console.ReadLine();
+            
+            //------------output------------
+            Console.WriteLine(name);
+            Console.WriteLine("1. " + class1 + " " + class1grade);
+            Console.WriteLine("2. " + class2 + " " + class2grade);
         }
     }
 }
