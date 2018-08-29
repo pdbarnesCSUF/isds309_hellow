2a883024498e1a89fd101ce3f6e99acd2848aea0
pdbarnes <pdbarnes@csu.fullerton.edu>
2018-08-28 17:14:25 -0700

    Add project files.

---
+++ hellow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hellow
+{
+    class Program
+    {
+        static void Pause()
+        {
+            Console.WriteLine("Press the anykey to continue.");
+            Console.ReadKey(true);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world! again");
+            string input;
+            input = Console.ReadLine();
+
+            Console.WriteLine("input:" + input);
+            Pause();
+        }
+    }
+}
